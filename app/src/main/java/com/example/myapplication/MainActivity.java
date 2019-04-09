841c3929428bb21f6ed27ce355076dc114f9a259
ehpior <ehpior@kookmin.ac.kr>
2019-04-09 16:46:30 +0900

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,29 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClick01(View v){
-        Intent intent1 = new Intent(this, Main2Activity.class);
-        EditText tv1 = findViewById(R.id.editText);
-        EditText tv2 = findViewById(R.id.editText2);
-
-        intent1.putExtra("String1",tv1.getText().toString());
-        intent1.putExtra("String2",tv2.getText().toString());
-
-        startActivityForResult(intent1,3000);
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(resultCode == RESULT_OK){
-            switch(requestCode){
-                case 3000:
-                    TextView kk = findViewById(R.id.textView3);
-                    kk.setText(data.getStringExtra("result"));
+        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            public void onSelectedDayChange(CalendarView view, int year,
+                                            int month, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, "" + year + "/" +
+                        (month + 1) + "/" + dayOfMonth, 0).show();
             }
-        }
+        });
     }
 
 }
